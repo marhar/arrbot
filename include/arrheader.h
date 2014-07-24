0183f7968310df821c50beeb250c33ce3563339e
Mark Harrison <mh@pixar.com>
2014-07-24 00:53:16 -0700

    needs some work to figure out include paths. now relies on symlink to /arrbot. unwisely moved pgm files to CamelCase names. is that the right thing? added servo control and config header. maybe need to rename arrheader to show config intention?

---
+++ include/arrheader.h
@@ -0,0 +1,63 @@
+#ifndef _ARHEADER_H
+#define _ARHEADER_H
+
+#include <Arduino.h>
+#include "/arrbot/arrservos.h"
+
+
+#define ARRBOT_MAGIC 0x7261  // magic number for this header ("ar")
+#define ARRBOT_HVERSION 1    // version number of this header
+struct arrbot_cfg
+{
+    unsigned int magic;    // magic number, is this good data?
+    unsigned int version;  // version of this config header
+    unsigned int sz;       // size of this header
+    unsigned int cksum;    // checksum of data following checksum
+    arrbot_servo  l;       // left servo
+    arrbot_servo  r;       // right servo
+    
+    void read();           // read header from eprom
+    void write();          // write header to eprom
+    void init();           // init header with proper values
+
+};
+#endif
+
+//----------------------------------------------------------------------
+void arrbot_cfg::read()
+{
+  unsigned int i;
+  unsigned char *p = (unsigned char*)this;
+
+  for (i = 0; i < sizeof(*this); ++i)
+    p[i] = EEPROM.read(i);
+}
+
+//----------------------------------------------------------------------
+void arrbot_cfg::write()
+{
+  unsigned int i;
+  unsigned char *p = (unsigned char*)this;
+
+  sz = sizeof(*this);
+  // TODO: figure out checksum
+  cksum = 0;
+
+  // TODO: only write modified bytes
+  // TODO: look at EEPROMex lib
+  for (i = 0; i < sizeof(*this); ++i)
+    EEPROM.write(i, p[i]);
+
+  // TODO: verify data which has been read
+}
+
+//----------------------------------------------------------------------
+void arrbot_cfg::init()
+{
+  this->magic = ARRBOT_MAGIC;
+  this->version = ARRBOT_HVERSION;
+  this->sz = sizeof(*this);
+  this->cksum = 0;  // TODO: put chksum here
+  this->l.init();
+  this->r.init();
+}
---
+++ include/arrservos.h
@@ -0,0 +1,22 @@
+#ifndef _ARSERVOS_H
+#define _ARSERVOS_H
+struct arrbot_servo
+{
+    int mid;     // midpoint of servo, where it doesn't move the wheel 
+    int span;    // how much mid can be {inc,dec}remented for full travel
+    int reverse; // is this servo reversed? 1 = no, -1 = yes
+
+    void init();
+};
+
+//----------------------------------------------------------------------
+// init -- initialize with some reasonable values (1000/1500/2000)
+//----------------------------------------------------------------------
+
+void arrbot_servo::init() {
+  this->mid = 1500;
+  this->span = 500;
+  this->reverse = 1;
+}
+
+#endif
