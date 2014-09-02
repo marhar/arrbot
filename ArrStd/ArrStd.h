f228eb7cea50fd028cf75b63d9cab2fbdd350e12
Mark Harrison <mh@pixar.com>
2014-09-02 13:45:03 -0700

    Some convenient stuff.

---
+++ ArrStd/ArrStd.h
@@ -0,0 +1,23 @@
+//----------------------------------------------------------------------
+// ArrStd.h -- some standard ArrBot utility things
+//----------------------------------------------------------------------
+
+#ifndef ARRBOT_BAUD
+//#define ARRBOT_BAUD 9600
+#define ARRBOT_BAUD 115200
+#endif
+
+//----------------------------------------------------------------------
+// some convenient printing macros
+//----------------------------------------------------------------------
+
+#define P(x) Serial.print(x)          // print
+#define NL(x) Serial.println(x)       // print line
+#define PS(x) P(x); P(" ")            // print with space
+#define PV(x) P(" " #x ": "); P(x);   // print a variable
+
+//----------------------------------------------------------------------
+// declarations for some handy things
+//----------------------------------------------------------------------
+
+extern void countdown(int);
